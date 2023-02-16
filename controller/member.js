719106bcd6ddbb53f2f88be4df03390aaabddab6
msjfa123 <mohammadsajadjafariazad@gmail.com>
2023-02-16 00:40:44 -0800

    add clg

--- controller/member.js
+++ controller/member.js
@@ -1,28 +1,31 @@
 const bcrypt = require("bcrypt");
 const member = require("../model/member");
+const multer = require('multer');
 
 exports.add = async(req,res,next)=>{
-    let{fullName,phoneNumber,password,age,imageName} = req.body
+  console.log("000000000000000000000000000000000000000000");
+  console.log(req.body);
+  console.log("888888888888888888888888888888888");
+    let{fullName,phoneNumber,password,age} = req.body
+  
 
     const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, __basedir + "/PUBLIC/upload/");
+      cb(null, __basedir + "/public/upload/");
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}---${file.originalname}`);
       },
-      
-})
-storage.filename()
-const uploadFile = multer({ storage: storage });
+    })
+    const uploadFile = multer({storage:storage}).single("save");
 
+    console.log("999999999999999999999999999999999999999999999999");
 
-
-member.create({
-    fullName:fullName,
-    phoneNumber:phoneNumber,
-    password:password,
-    age:age,
-    imageName:uploadFile
-})
+    await member.create({
+      fullName:fullName,
+      phoneNumber:phoneNumber,
+      password:password,
+      age:age,
+      imageName:uploadFile.fullName
+  })
 }
